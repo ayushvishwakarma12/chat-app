4f7cddf34ed44a0e7dc9ceb535649fc6d202f7d0
Ayush Vishwakarma <ayushvishwakarma114@gmail.com>
2024-03-04 18:07:13 +0530

    Add backend login signup and logout functionality

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`server is running on port ${PORT}`);
+});
