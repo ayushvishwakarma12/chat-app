7f4d037690026615bec2d7227397282d4c0c754c
Ayush Vishwakarma <ayushvishwakarma114@gmail.com>
2024-03-08 18:43:11 +0530

    Update vercel.json file

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 const __dirname = path.resolve();
 
@@ -34,7 +34,14 @@ app.use("/api/users", userRoutes);
 app.use(express.static(path.join(__dirname, "frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(
+    path.join(__dirname, "frontend", "dist", "index.html"),
+    function (error) {
+      if (error) {
+        res.status(500).send(error);
+      }
+    }
+  );
 });
 
 // app.get("/", (req, res) => {
